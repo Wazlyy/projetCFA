7d9332392e074aea5bc8d363318cead92bb2138e
Wazlyy <58254367+Wazlyy@users.noreply.github.com>
2020-03-09 11:14:47 +0100

    INIT

---
+++ connexion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>CFA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+
+</head>
+<body>
+<div class="container2">
+    <h3 class="demarrer">Démarrer</h3>
+    <img class="home" src="img/homelogo.png" width="100px">
+    <h3 class="connect"> Connecter vous à votre espace</h3>
+
+    <form action="verification.php" method="POST">
+    <input  type="text" placeholder="Email" name="email" class="email" required>
+    <input type="password" placeholder="Mot de passe" name="password" class="password" required>
+    </form> 
+    <div class="button__holder">
+        <button class="plus"></button>
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>CFA</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        
+        <div class="container">
+        
+            <img class="logo_declic" src="img/logo.png" alt="logo_declic" />
+
+            <h1 class="titre1">DECLIC</h1>
+
+            <h2 class="titre2">SOYEZ ENTREPRENEUR DE VOTRE VIE</h2>
+
+            <div class="zone_btn">
+                <button type="button" class="btn_inscription"><a href="">Inscription</a></button>
+                <button type="button" class="btn_connexion"><a href="">Connexion</a></button>
+            </div>
+        
+        </div>
+
+        <footer class="partenaires">
+            <img class="partenaire1" src="img/simplonlogo.png" />
+            <img class="partenaire2" src="img/banquelogo.png" />
+            <img class="partenaire3" src="img/cfailogo.png" />
+            <img class="partenaire4" src="img/eole.jpg" />
+        </footer>
+        
+        <script src=""></script>
+    </body>
+</html>
