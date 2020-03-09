7e7e4dd61ce242a44545119f1739601632b72733
Wazlyy <antoineplay11@gmail.com>
2020-03-09 15:18:24 +0100

    v2.1

---
+++ inscription.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>CFA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    
+
+</head>
+<body>
+<div class="container3">
+    <h3 class="newCompte">Nouveau Compte</h3>
+    <img class="" src="img/piecejointe.png" width="100px">
+    <h3 class="telechargement"> Télécharger une photo de profil</h3>
+
+    <form action="verification.php" method="POST">
+    
+    <input  type="text" placeholder="Nom" name="nominsc" class="nominsc" required>
+    <input  type="text" placeholder="Prénom" name="prenominsc" class="prenominsc" required>
+    <input  type="text" placeholder="Email" name="emailinsc" class="emailinsc" required>
+    <input type="password" placeholder="Mot de passe" name="passwordinsc" class="passwordinsc" required>
+    </form> 
+    
+    <div class="button__holder">
+        <button class="plus"></button>
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
