f97f98f4374d940fea0e3825982a71c7727e39b3
Wazlyy <antoineplay11@gmail.com>
2020-03-09 16:01:11 +0100

    v2.3

--- index.php
+++ index.php
@@ -1,36 +1,35 @@
 <!DOCTYPE html>
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>CFA</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="css/index.css">
-    </head>
-    <body>
-        
-        <div class="container">
-        
-            <img class="logo_declic" src="img/logo.png" alt="logo_declic" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>CFA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
 
-            <h1 class="titre1">DECLIC</h1>
+<body>
 
-            <h2 class="titre2">SOYEZ ENTREPRENEUR DE VOTRE VIE</h2>
-
-            <div class="zone_btn">
-                <button type="button" class="btn_inscription"><a href="inscription.php">Inscription</a></button>
-                <button type="button" class="btn_connexion"><a href="connexion.php">Connexion</a></button>
-            </div>
-        
+    <div class="container-fluid">
+        <img class="logo_declic" src="img/logo.png" alt="logo_declic" />
+        <h1 class="titre1">DECLIC</h1>
+        <h2 class="titre2">SOYEZ ENTREPRENEUR DE VOTRE VIE</h2>
+        <div class="zone_btn">
+            <button type="button" class="btn_inscription"><a href="inscription.php">Inscription</a></button>
+            <button type="button" class="btn_connexion"><a href="connexion.php">Connexion</a></button>
         </div>
+    </div>
 
-        <footer class="partenaires">
-            <img class="partenaire1" src="img/simplonlogo.png" />
-            <img class="partenaire2" src="img/banquelogo.png" />
-            <img class="partenaire3" src="img/cfailogo.png" />
-            <img class="partenaire4" src="img/eole.jpg" />
-        </footer>
-        
-        <script src=""></script>
-    </body>
+    <footer class="partenaires">
+        <img class="partenaire1" src="img/simplonlogo.png" />
+        <img class="partenaire2" src="img/banquelogo.png" />
+        <img class="partenaire3" src="img/cfailogo.png" />
+        <img class="partenaire4" src="img/eole.jpg" />
+    </footer>
+
+    <script src=""></script>
+</body>
+
 </html>
