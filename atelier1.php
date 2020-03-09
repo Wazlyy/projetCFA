ae1f9a3ef1629444574fe9161942295821193432
Wazlyy <antoineplay11@gmail.com>
2020-03-09 15:07:23 +0100

    v2

---
+++ atelier1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>CFA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+    
+<body>
+<div class="container4">
+    <h3 class="atelier1">Atelier1<br>Rencontre avec des entrepreneurs</h3>
+    <img class="logo2" src="img/logo2.png" width="150px">
+    <h3 class="ordre">A quel entrepreneur vous identifiez vous
+        le plus ? Classer par ordre de<br>
+        préférence.</h3>
+
+
+        <button type="button" class="btn entreprise1">Entreprise 1</button>
+        <button type="button" class="btn entreprise2">Entreprise 2</button>
+        <button type="button" class="btn entreprise3">Entreprise 3</button>
+        <button type="button" class="btn entreprise4">Entreprise 4</button>
+        <button type="button" class="btn entreprise4">Entreprise 5</button>
+    
+    <div class="button__holder">
+        <button class="plus"></button>
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
