047650bb5c5dfcdb9530b3e39a627f18165a3f7f
Moonrockxx <thomasferre6@gmail.com>
2020-03-10 10:35:41 +0100

    v2.6

--- atelier2.php
+++ atelier2.php
@@ -11,31 +11,33 @@
     </head>
     <body>
 
-        <div class="container">
+        <div class="container-fluid" id="ctnr_at2">
             <div class="row">
-                <div class="col-12" class="zone_back">
+                <div class="zone_back" class="col-12">
                     <img class="fleche_back_a2" src="" alt="fleche_retour" />
                 </div>
-                <div class="col-12" class="zone_titre1_atelier2">
-                    <h1></h1>
+                <div class="zone_titre1_atelier2" class="col-12">
+                    <h1>Atelier 2 : En route pour Paris-Daker et l'île déserte</h1>
                 </div>
-                <div class="col-12" class="zone_boussole_atelier2">
+                <div class="zone_boussole_atelier2" class="col-12">
                     <img class="img_boussole" src="" alt="boussole" />
-                    <p></p>
+                    <p>Graduer ces affirmations en faisant glisser le curseur</p>
                 </div>
-                <div class="col-12" class="zone_curseur_atelier2">
-                    <h2 class="consigne_at2"></h2>
-                    <!-- Curseur à mettre ici -->
+                <div class="zone_curseur_atelier2" class="col-12">
+                    <h2 class="consigne_at2">Vous avez été plutôt organisé(e) :</h2>
+                    <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+                    <p>Value: <span id="demo"></span></p>
                 </div>
                 <div class="col-12" class="pagination_atelier2">
-                    <!-- Pagination à implémenter -->
+                    
                 </div>
             </div>
         </div>
         
-        <script src="" async defer></script>
+        
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="js/function.js" type="text/javascript"></script>
     </body>
 </html>
