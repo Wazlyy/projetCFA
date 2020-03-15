d27b9491e753d908860ccbc66b7a3b283db71af2
Moonrockxx <thomasferre6@gmail.com>
2020-03-15 17:04:59 +0100

    v4.6

---
+++ atelier3.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Aller plus loin</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/atelier3.css">
+    </head>
+    <body>
+
+        <section class="container-fluid ctnr_at3">
+            <div class="row">
+                
+                <div class="col-12 zone_retour">
+                    <img class="fleche_retour" src="img/icon-gauche.png" alt="fleche_retour" />
+                </div>
+
+                <div class="col-12 zone_titre_at3">
+                    <h1 class="titre1_at3">Atelier 3 : Aller plus loin...</h1>
+                    <svg class="img_at3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80.14 45.34"><defs><style>.cls-1{fill:#4cbfdd;fill-rule:evenodd;}</style></defs><title>Fichier 2</title><g id="Calque_2" data-name="Calque 2"><g id="Calque_1-2" data-name="Calque 1"><path class="cls-1" d="M75.12,227.26a4.66,4.66,0,0,1-.45.75q-3.66,3.66-7.31,7.33l-5.75,5.82c-2.42,2.46-4.9,4.9-7.35,7.36l-4.64,4.69c-3.15,3.18-6.33,6.36-9.49,9.55a1.08,1.08,0,0,1-1.58.17c-1.22-.81-2.42-1.63-3.62-2.45-1.42-1-2.81-1.91-4.2-2.88a.35.35,0,0,0-.47,0c-1.08.88-2.13,1.75-3.2,2.61-1.49,1.2-2.94,2.39-4.44,3.59s-2.75,2.24-4.12,3.36c-1.68,1.34-3.3,2.69-5,4a1,1,0,0,1-1.68-.6,3.11,3.11,0,0,1-.08-.44c-.47-3-1-5.95-1.44-8.92-.43-2.67-.87-5.33-1.31-8-.24-1.49-.47-3-.71-4.46q-.48-3-1-6a.68.68,0,0,0-.28-.43L.69,238.58c-1.73-1-3.46-2.07-5.17-3.12A1,1,0,0,1-5,234.31a1.09,1.09,0,0,1,.94-.81c.54-.06,1.07-.1,1.6-.14,1.62-.17,3.24-.31,4.9-.47s3.59-.34,5.41-.53l4.08-.36,4.76-.47c1.59-.15,3.18-.3,4.77-.43l5.67-.54,5.36-.51,5.64-.54c1.6-.13,3.2-.28,4.8-.43,1.83-.18,3.67-.37,5.51-.54l4.75-.44,4.76-.47c1.85-.17,3.68-.35,5.57-.51l5.6-.54c1.54-.13,3.11-.29,4.68-.45A1.09,1.09,0,0,1,75,227.21s.08,0,.08,0m-8.06,2.59c-3,.67-6,1.38-9.05,2-3.42.72-6.82,1.66-10.2,2.63l-13.7,3.79c-2.91.8-5.77,1.6-8.64,2.41-2.59.7-5.16,1.44-7.74,2.15l-4.81,1.31a2.43,2.43,0,0,0,.23.23c2.4,1.64,4.83,3.27,7.23,4.89L28,254.5c2.46,1.66,4.93,3.33,7.34,5,1.25.84,2.46,1.66,3.65,2.5.21.11.26.09.42,0,2.07-2.12,4.17-4.23,6.27-6.34,2.69-2.67,5.32-5.36,8-8.05L58,243.27c2-2,4-4,5.93-5.92l8.42-8.49c0-.06.13-.13.24-.26-1.89.44-3.68.85-5.49,1.25m-69.78,5.46c2.91,1.73,5.8,3.47,8.7,5.22.69.43,1.4.84,2.12,1.26a.58.58,0,0,0,.41.05c2.78-.61,5.53-1.23,8.29-1.85,2.06-.44,4.12-.9,6.16-1.35,3.68-.82,7.35-1.67,11-2.49q6.7-1.52,13.46-3c3.47-.79,7-1.57,10.44-2.33,4.76-1.08,9.55-2.13,14.31-3.2a7.06,7.06,0,0,0,1.18-.34l-77.15,7.28,0,0c.37.25.73.48,1.1.71m15.61,35c5.56-4.52,11.12-9,16.68-13.53C24,253,18.45,249.27,12.89,245.47ZM8.43,243c1.13,6.81,2.2,13.56,3.31,20.34h0V244.36a1.15,1.15,0,0,1,1-1.27l9.51-2.64,8.21-2.31a1.38,1.38,0,0,0,.22-.1V238L8.43,243" transform="translate(5.02 -226.12)"/></g></g></svg>                    
+                    <p class="instruction_at3">Graduer ces affirmations en faisant <br /> glisser le curseur</p>
+                </div>
+
+                <div class="col-12 zone_slider_at3">
+                    <h2 class="txt_slider_at3">Vous avez été plutôt :</h2>
+                    <div class="col-12 multislider_at3">
+                        <p>- Déterminé(e) :</p>
+                        <!-- Slider ici -->
+
+                        <p>- A l'écoute des autres :</p>
+                        <!-- Slider ici -->
+
+                        <p>- Au delà de vos limites :</p>
+                        <!-- Slider ici -->
+                    </div>
+                </div>
+
+                
+
+                <div class="col-12 zone_button_next">
+                    <svg class="button_next_at3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2.06 2.06"><defs><style>.cls-1{fill:#4cbfdd;}.cls-2{fill:#fff;fill-rule:evenodd;}</style></defs><title>Fichier 2</title><g id="Calque_2" data-name="Calque 2"><g id="Calque_1-2" data-name="Calque 1"><path class="cls-1" d="M2.06,1A1,1,0,1,1,1,0,1,1,0,0,1,2.06,1Z"/><path class="cls-2" d="M1.17,1.07.61.51h.57l.53.53,0,0a.12.12,0,0,0-.05.05l-.5.49H.62Z"/></g></g></svg>
+                </div>
+            </div>
+        </section>
+        
+        
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="js/atelier3.js" type="text/javascript"></script>
+    </body>
+</html>
